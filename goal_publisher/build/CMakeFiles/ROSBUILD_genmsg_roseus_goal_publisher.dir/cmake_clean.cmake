b9208e4878dc3042f303a9b356860b7dd2062167
bakui <bakui@bakui-ThinkPad-W510.(none)>
2013-12-25 00:39:51 +0900

    delete unnecessary files and directroy for checkout

--- goal_publisher/build/CMakeFiles/ROSBUILD_genmsg_roseus_goal_publisher.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/ROSBUILD_genmsg_roseus_goal_publisher"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/ROSBUILD_genmsg_roseus_goal_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- goal_publisher/build/CMakeFiles/ROSBUILD_gensrv_roseus_goal_publisher.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/ROSBUILD_gensrv_roseus_goal_publisher"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/ROSBUILD_gensrv_roseus_goal_publisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
