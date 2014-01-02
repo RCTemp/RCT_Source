286563a6991a15c8e03a59640d6840308e563158
noda <noda@noda-ThinkPad-T430.(none)>
2014-01-02 16:12:11 +0900

    ros2osc receive simple s equation, such as <adress> float|string|boolean ..

--- ROS2OSC/scripts/simple_pub.sh
+++ ROS2OSC/scripts/simple_pub.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-while [ true ] ;
-do
-    for pub in $@ ;
-    do
-	echo "[simple_pub.sh] rostopic pub -1 $pub ;" ;
-	rostopic pub -1 `echo $pub | tr "[;|]" "[ ]"` ;
-    done ;
-done ;
+rostopic pub -r 3 /ros2osc/$1 $2 "$3 $4"
+# while [ true ] ;
+# do
+#     for pub in $@ ;
+#     do
+# 	echo "[simple_pub.sh] rostopic pub -1 `echo $pub | tr \"[;]\" \"[ ]\"`;" ;
+# 	rostopic pub -1 `echo $pub | tr "[;]" "[ ]"` ;
+#     done ;
+# done ;
