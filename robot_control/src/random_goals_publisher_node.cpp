c251621ff21620d227efa7298ee9bd86f3de2714
Shun Lee <ijun.slee@gmail.com>
2014-01-02 23:24:55 +0900

    create robot_control package

---
+++ robot_control/src/random_goals_publisher_node.cpp
@@ -0,0 +1,18 @@
+#include "../include/random_goals.h"
+
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "random_goals_publisher");
+  random_goal RandomGoal;
+  ros::Rate rate(1);
+  
+  while(ros::ok()){
+    RandomGoal.random_make_x();
+    RandomGoal.random_make_y();
+    RandomGoal.makegoal();
+    RandomGoal.goalpublish();
+    rate.sleep();
+  }
+  return 0;
+}
+  
---
+++ robot_control/src/test_publisher.cpp
@@ -0,0 +1,68 @@
+#include "../include/ros_header.h"
+
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "test_publisher");
+  ros::NodeHandle nh;
+  ros::Publisher flag_publisher  = nh.advertise<std_msgs::Bool>("flag_from_master",1);
+  std_msgs::Bool flag;
+  bool tmp_flag;
+  int get_flag;
+  tmp_flag =true;
+  // while(tmp_flag){
+  std::cout<<"Put flag 1 or 0"<<std::endl;
+  std::cin>>get_flag;
+  std::cout<<get_flag<<std::endl;
+  if(get_flag == 1){
+ flag.data = true;
+ tmp_flag = true;
+  }else if(get_flag == 0){
+ flag.data =false;
+ tmp_flag=false;
+ if(!flag.data) std::cout<<"ffafafa"<<std::endl;
+  }
+  if(!tmp_flag) std::cout<<"jsjsjsjsj"<<std::endl;
+  // else{ break;}
+  //std::cout<<"Flag = "<<flag.data<<std::endl;
+  flag_publisher.publish(flag);
+  // }
+
+  //  tmp_flag = flag.data;
+  // std::cout<<"tmp_flag = "<<tmp_flag<<std::endl;
+
+  /*  
+if( !(tmp_flag==0 || tmp_flag==1)) {
+    std::cout<<"Please put 0 or 1"<<std::endl;
+  }
+  */
+
+
+    
+  std::cout<<"deta"<<std::endl;
+
+  nav_msgs::Path path;
+  ros::Publisher path_pub_ = nh.advertise<nav_msgs::Path>("path_from_UI",1);
+  while(ros::ok()){ 
+ path_pub_.publish(path);
+  }
+    ros::spin();
+ 
+    
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
