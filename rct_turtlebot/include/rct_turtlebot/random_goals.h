9f43d6d883bc2af37a353c6331c9293c362fd050
Moju Zhao <chou@jsk.imi.i.u-tokyo.ac.jp>
2014-01-08 16:07:03 +0900

    shift the files of turtlebot_control to turtlebot_control

--- rct_turtlebot/include/rct_turtlebot/random_goals.h
+++ rct_turtlebot/include/rct_turtlebot/random_goals.h
@@ -1,4 +1,10 @@
-#include "turtlebot_control/ros_header.h"
+#include <ros/ros.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
+#include <std_msgs/Empty.h>
+#include <geometry_msgs/PoseStamped.h>
 #include "tf/transform_broadcaster.h"
 
 #define RANDMAX 20
